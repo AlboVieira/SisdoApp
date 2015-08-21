467a658354346981c2a580d2b9a6a7420e760319
albovieira <aalbovieira@gmail.com>
2015-08-20 21:40:06 -0300

    arquivos

---
+++ app/src/main/java/br/com/albovieira/sisdoapp/InstituicoesActivityList.java
@@ -0,0 +1,8 @@
+package br.com.albovieira.sisdoapp;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+
+public class InstituicoesActivityList extends ListActivity {
+
+}
---
+++ app/src/main/java/br/com/albovieira/sisdoapp/UsuarioActivity.java
@@ -0,0 +1,10 @@
+package br.com.albovieira.sisdoapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class UsuarioActivity extends AppCompatActivity {
+
+}
